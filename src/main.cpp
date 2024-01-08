5757ab6258711e6d9f28c55326298fb08487619f
Sunglas <diego.estrada1@proton.me>
2024-01-08 17:09:02 -0400

    [fix] readme renamed wrongly and main had wrong example

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,7 @@
 #include "prelude.hpp"
 
 /* === examples === */
-/* test accuracy of type sizes */
-/*
-#include <iostream>
-#include <format>
+// test accuracy of type sizes
 
 proc Main([[maybe_unused]] std::span<const std::string_view> args) noexcept -> I32
 {
@@ -30,41 +27,5 @@ proc Main([[maybe_unused]] std::span<const std::string_view> args) noexcept -> I
 	ignore = println(std::format("D128: {}", CHAR_BIT * sizeof(D128)));
 
 
-	return 0;
-}
-*/
-#include <random>
-#include "prelude.hpp"
-
-proc generate(I32 n, Char left = '[', Char right = ']') -> String
-{
-	var rd = std::random_device();
-	var str = String(n, left) + String(n, right);
-	std::shuffle(str.begin(), str.end(), std::mt19937(rd()));
-
-	return str;
-}
-
-fn balanced(std::string_view str, Char left = '[', Char right = ']') -> Bool
-{
-	var count = 0;
-	for (let& ch : str) {
-		if (ch == left)
-			count++;
-		else if (ch == right)
-			if (--count < 0)
-				return false;
-	}
-
-	return count == 0;
-}
-
-proc Main([[maybe_unused]] const std::span<const std::string_view> args) -> I32
-{
-	for (var i = 0; i < 9; ++i) {
-		let s = String(generate(i));
-		ignore = println(
-		    std::format(" {}: {}", balanced(s) ? "ok" : "bad", s));
-	}
 	return 0;
 }
