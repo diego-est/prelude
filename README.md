ab33e27363bdf4c99c4de36dd6dcc8dfee649245
Sunglas <diego.estrada1@proton.me>
2024-01-08 18:04:20 -0400

    [fix] typo in README.md

--- README.md
+++ README.md
@@ -116,11 +116,7 @@ standard library, for example `std::unique_ptr<T>`.
 | `var`     | `auto` |
 | `fn`      | `[[nodiscard, gnu::const]] auto` |
 | `proc`    | `[[nodiscard]] auto` |
-<<<<<<< HEAD
-| `ignored = foo();` | `static_cast<void>(foo());`
-=======
-| `ignore = foo()` | `static_cast<void>(foo())`
->>>>>>> 5a536c35cf60085b9709a71aa2b5240961122046
+| `ignored = foo();` | `static_cast<void>(foo());` |
 | `fst`     | `std::get<0>` |
 | `snd`     | `std::get<1>` |
 | `thr`     | `std::get<2>` |
--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,13 @@
 
 proc Main([[maybe_unused]] std::span<const std::string_view> args) noexcept -> I32
 {
-	ignore = println(std::format("BF16: {}", CHAR_BIT * sizeof(BF16)));
-	ignore = println(std::format("float: {}", CHAR_BIT * sizeof(float)));
-	ignore = println(std::format("F32: {}", CHAR_BIT * sizeof(F32)));
-	ignore = println(std::format("D32: {}", CHAR_BIT * sizeof(D32)));
-	ignore = println(std::format("double: {}", CHAR_BIT * sizeof(double)));
-	ignore = println(std::format("F64: {}", CHAR_BIT * sizeof(F64)));
-	ignore = println(std::format("D64: {}", CHAR_BIT * sizeof(D64)));
-	// Err: F80 is the same as F128
-	// ignore = println(std::format("F80: {}", CHAR_BIT * sizeof(F80)));
-	ignore = println(std::format("long double: {}", CHAR_BIT * sizeof(long double)));
-	ignore = println(std::format("F128: {}", CHAR_BIT * sizeof(F128)));
-	ignore = println(std::format("D128: {}", CHAR_BIT * sizeof(D128)));
+	var s = String();
+	while (true) {
+		ignore = print(s);
+		var a = getLine();
+		s+= "Give me two floating point numbers: ";
+		s += a;
+	}
 
 
 	return 0;
